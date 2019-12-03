3d2c6cd5bda4793f24e1bc2c1c9de1cf022dab62
pegah-m-sadeghi <pegah.m.sadeghi@gmail.com>
2019-12-03 11:20:30 -0500

    Update CheaterStrategy.java
    Add javadoc for cheater.

--- Risk/src/main/java/model/CheaterStrategy.java
+++ Risk/src/main/java/model/CheaterStrategy.java
@@ -4,11 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.util.Pair;
-
+/**
+ * This class implements cheater.
+ *  
+ *  @author Pegah
+ */
 public class CheaterStrategy implements StrategyInterface{
 
 	private int maxPossibleAryCount =1000;
-	
+	/**
+	 * 
+	 * this method is used for the reinforcement phase.
+	 * 
+	 * @param Player : player this is an object of Player
+	 * @param Country : reinforcementCountry this is an object of Country
+	 * @param int : noOfArmies this is number of armies
+	 * @param PhaseViewModel : phaseViewModel this is an object of PhaseViewModel
+	 */
 	@Override
 	public void reinforcement(Player player, Country reinforcementCountry, int noOfArmies,
 			PhaseViewModel phaseViewModel) {
@@ -26,7 +38,16 @@ public class CheaterStrategy implements StrategyInterface{
 		
 		player.setArmy(0);
 	}
-
+	/**
+	 * 
+	 * this method is used for the fortification phase.
+	 * 
+	 * @param Player : player this is an object of Player
+	 * @param Country : fromCountry this is an object of Country
+	 * @param Country : toCountry this is an object of Country
+	 * @param int : armiesToMove this is the number of armies to move
+	 * @param PhaseViewModel : phaseViewModel this is an object of PhaseViewModel 
+	 */
 	@Override
 	public void fortify(Player player, Country fromCountry, Country toCountry, int armiesToMove,
 			PhaseViewModel phaseViewModel) {
@@ -46,7 +67,20 @@ public class CheaterStrategy implements StrategyInterface{
 		}
 		
 	}
-
+	/**
+	 * 
+	 * this method is used for the attack phase.
+	 * 
+	 * @param Player : attacker this is an object of Player
+	 * @param Country : attackerCountry this is an object of Country
+	 * @param Country : defenderCountry this is an object of Country
+	 * @param Player : defender this is an object of Player
+	 * @param boolean : ifAllOut this check if all the armies out or not
+	 * @param int : totalAttackerDice this calculate the total number of attacker dices
+	 * @param int : totalDefenderDice this calculate the total number of defender dices
+	 * @param PhaseViewModel : phaseViewModel this is an object of PhaseViewModel 
+	 * @return Pair: this returns the countries who win the game
+	 */
 	@Override
 	public Pair<Boolean, Integer> attack(Player attacker, Country attackerCountry, Country defenderCountry,
 			Player defender, boolean ifAllOut, int totalAttackerDice, int totalDefenderDice,
