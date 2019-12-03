a2a02b33d74ba94efdbef959abd0d0ffe51334fb
pegah-m-sadeghi <pegah.m.sadeghi@gmail.com>
2019-12-03 11:21:43 -0500

    Update RandomStrategy.java
    Add javadoc for random.

--- Risk/src/main/java/model/RandomStrategy.java
+++ Risk/src/main/java/model/RandomStrategy.java
@@ -9,9 +9,23 @@ import java.util.Random;
 
 import ControllerHelper.MapControllerHelper;
 import javafx.util.Pair;
-
+/**
+ * 
+ * This class implements random.
+ * 
+ * @author Pegah
+ *
+ */
 public class RandomStrategy implements StrategyInterface {
-
+	/**
+	 * 
+	 * this method is used for the reinforcement phase.
+	 * 
+	 * @param Player : player this is an object of Player
+	 * @param Country : reinforcementCountry this is an object of Country
+	 * @param int : noOfArmies this is number of armies
+	 * @param PhaseViewModel : phaseViewModel this is an object of PhaseViewModel
+	 */
 	@Override
 	public void reinforcement(Player player, Country reinforcementCountry, int noOfArmies,
 			PhaseViewModel phaseViewModel) {
@@ -28,7 +42,14 @@ public class RandomStrategy implements StrategyInterface {
 	}
 
 	/**
-	 * thoda theek karna hai. 
+	 * 
+	 * this method is used for the fortification phase.
+	 * 
+	 * @param Player : player this is an object of Player
+	 * @param Country : fromCountry this is an object of Country
+	 * @param Country : toCountry this is an object of Country
+	 * @param int : armiesToMove this is the number of armies to move
+	 * @param PhaseViewModel : phaseViewModel this is an object of PhaseViewModel 
 	 */
 	@Override
 	public void fortify(Player player, Country fromCountry, Country toCountry, int armiesToMove,
@@ -80,7 +101,20 @@ public class RandomStrategy implements StrategyInterface {
 			}
 		}
 	}
-
+	/**
+	 * 
+	 * this method is used for the attack phase.
+	 * 
+	 * @param Player : attacker this is an object of Player
+	 * @param Country : attackerCountry this is an object of Country
+	 * @param Country : defenderCountry this is an object of Country
+	 * @param Player : defender this is an object of Player
+	 * @param boolean : ifAllOut this check if all the armies out or not
+	 * @param int : totalAttackerDice this calculate the total number of attacker dices
+	 * @param int : totalDefenderDice this calculate the total number of defender dices
+	 * @param PhaseViewModel : phaseViewModel this is an object of PhaseViewModel 
+	 * @return Pair: this returns the countries who win the game
+	 */
 	@Override
 	public Pair<Boolean, Integer> attack(Player attacker, Country attackerCountry, Country defenderCountry,
 			Player defender, boolean ifAllOut, int totalAttackerDice, int totalDefenderDice,
@@ -161,16 +195,11 @@ public class RandomStrategy implements StrategyInterface {
 	/**
 	 * This method is helper method for attack()
 	 * 
-	 * @param attackerCountry
-	 *            : attacker country
-	 * @param defenderCountry
-	 *            : defender country
-	 * @param attackerDiceResult
-	 *            : attacker dice result
-	 * @param defenderDiceResult
-	 *            : defender dice result
-	 * @param phaseViewModel
-	 *            : phase view model
+	 * @param attackerCountry : attacker country this is an object of Country
+	 * @param defenderCountry : defender country this is an object of Country
+	 * @param attackerDiceResult : attacker dice result this is a list of attacker dices
+	 * @param defenderDiceResult : defender dice result this is a list of defender dices
+	 * @param phaseViewModel : phase view model this is an object of PhaseViewModel
 	 * @return leftTroops : get the left troop
 	 */
 	private int attackHelper(Country attackerCountry, Country defenderCountry, List<Integer> attackerDiceResult,
@@ -208,10 +237,8 @@ public class RandomStrategy implements StrategyInterface {
 	/**
 	 * diceRollResult() to return results of dice
 	 * 
-	 * @param armyCount
-	 *            : army count
-	 * @param ifAttacker
-	 *            : current attacker
+	 * @param armyCount : army count this is the count of armies
+	 * @param ifAttacker : this is the current attacker
 	 * @return result : get the result
 	 */
 	private List<Integer> diceRollResult(int armyCount, boolean ifAttacker) {
@@ -241,7 +268,11 @@ public class RandomStrategy implements StrategyInterface {
 		}
 		return result;
 	}
-
+	/**
+	 * this method is used for finding random numbers
+	 * @param int : size this is a size of given list
+	 * @return returns a random number
+	 */
 	private int randomInt(int size) {
 		Random random = new Random();
 		return random.nextInt(size);
